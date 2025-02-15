3491ea59d0ab3227b0332a2fc52d1a5346a1f53d
Dre <>
2025-02-15 12:00:58 +0300

    feat: public const for Actions

--- AGU_text_quest/MainLogic/Actions/FirstAction.cs
+++ AGU_text_quest/MainLogic/Actions/FirstAction.cs
@@ -8,7 +8,7 @@ namespace MainLogic
 {
 	public abstract class FirstAction : ActionBase
 	{
-		private const int ACTION_NUMBER = 1;
+		public const int ACTION_NUMBER = 1;
 		public override int Number => ACTION_NUMBER;
 	}
 }
