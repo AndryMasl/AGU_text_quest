2257c927776378ea4a92cb7dc7f914ef9646c2c6
Dre <>
2025-02-15 12:00:22 +0300

    refactor: комент

--- AGU_text_quest/MainLogic/Actions/ActionBase.cs
+++ AGU_text_quest/MainLogic/Actions/ActionBase.cs
@@ -11,7 +11,14 @@ namespace MainLogic
 		public abstract int Number { get; }
 		public abstract string ActionDescription { get; }
 		public bool IsVisible { get; set; }
+		/// <summary>
+		/// При true - необходимо задать следующий <see cref="PointBase"/> в свойство <see cref="NextPointID"/>. 
+		/// При false - смена <see cref="PointBase"/> не происходит.
+		/// </summary>
 		public bool IsAvailable { get; set; }
+		/// <summary>
+		/// Выводится  при <see cref="IsAvailable"/> == false
+		/// </summary>
 		public string? MassageAfterAction { get; set; }
 		public int NextPointID { get; set; }
 
