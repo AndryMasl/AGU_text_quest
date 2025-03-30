ce7df1b388aec31aacb46299141050f22fff1bad
Dre <>
2025-03-30 12:40:44 +0300

    feat: SeventhAction

---
+++ AGU_text_quest/MainLogic/Actions/SeventhAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainLogic
+{
+	public abstract class SeventhAction : ActionBase
+	{
+		public const int ACTION_NUMBER = 7;
+		public override int Number => ACTION_NUMBER;
+	}
+}
